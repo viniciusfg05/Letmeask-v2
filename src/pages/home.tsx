10324a144e86581cee166f585aca2ef94b655645
Vinicius Ferreira <vinicius.fg05@gmail.com>
2022-02-10 19:15:36 -0300

    Auth com o google Ok

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,5 +1,7 @@
 import { BrowserRouter as Router, useNavigate } from "react-router-dom";
 
+import { auth, firebase } from "../services/firebase"
+
 import illustrationImg from "../assets/images/illustration.svg";
 import logoImg from "../assets/images/logo.svg";
 import googleIconImg from "../assets/images/google-icon.svg";
@@ -15,7 +17,14 @@ export function Home() {
   
 
   function handleCreteRoom() {
-    history("/new/room")
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider).then(result => {
+      console.log(result);  
+
+      history("/new/room")
+    })
+
+
   }
   return (
     <div id="page-auth">
--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,19 +1,20 @@
-import firebase from 'firebase/app';
-
-import 'firebase/auth';
-import 'firebase/database';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID
+  apiKey: "AIzaSyA4JifyFUJBhgyL_D_taXB7SGswrwnIPuo",
+  authDomain: "letmeask-b337f.firebaseapp.com",
+  databaseURL: "https://letmeask-b337f-default-rtdb.firebaseio.com",
+  projectId: "letmeask-b337f",
+  storageBucket: "letmeask-b337f.appspot.com",
+  messagingSenderId: "617521980094",
+  appId: "1:617521980094:web:4656807d571de978e574ef"
 };
 
 firebase.initializeApp(firebaseConfig);
 
-export const auth = firebase.auth();
-export const database = firebase.database();
+const auth = firebase.auth();
+const database = firebase.firestore();
+
+export { firebase, auth, database };
